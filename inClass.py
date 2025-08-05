d8b03f290b8b0cf62c6ae1907a47f48f1a9cdf9b
Devkumar Nirmal <devkumar.nirmal@everestek.com>
2025-08-05 10:34:22 +0530

    modified inClass.py

--- inClass.py
+++ inClass.py
@@ -5,25 +5,24 @@ class Student:
         self.rollno = rollno
 
     def show(self):
-        print("name:",self.name,",","rollno:",self.rollno)
+        print("Name:",self.name,",","Rollno:",self.rollno)
 
     class Laptop:
 
-        def __init__(self, brand, cpu, ram):
-            self.brand = brand
+        def __init__(self, name, cpu, ram):
+            self.name = name
             self.cpu = cpu
             self.ram = ram
 
         def show(self):
-            print("brand:",self.brand,",","cpu:",",",self.cpu,",","ram:",self.ram)
+            print(f"name: {self.name}, cpu: {self.cpu}, ram:{self.ram}")
 
-s1 = Student('Dev','21CE1157')
+
+
+s1 = Student('Dev', '21CE1157')
 s2 = Student('Archana','24CA1024')
 
-
-lap1 = s1.Laptop('Dell','i5', 8)
+lap1 = s1.Laptop('Dell','i5',8)
 lap2 = s2.Laptop('Hp','i5',16)
 
-lap1.show()
-lap2.show()
-
+print(lap1.show())
